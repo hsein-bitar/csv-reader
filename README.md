454398c8d5bb93d571a8b6dccdc523a6e235e855
Hsein Bitar <hseinbitar22@gmail.com>
2022-07-18 23:42:10 +0300

    finished

---
+++ README.md
@@ -0,0 +1,48 @@
+# BTC USD Challenge Solution - Hsein Bitar
+
+<br>
+
+## Version
+1.0.0
+
+<br>
+
+<hr color=red>
+
+<br>
+
+<h1>Usage</h1>
+
+## pre-requisites: NodeJS installed on your device, this script has no other dependencies
+
+<br>
+
+## 1 - Extract the contents of this compressed folder
+
+<br>
+
+## 2 - Open your terminal of choice in the root of this script
+
+<br>
+
+## 3 - Run the script with nodeJS
+
+```sh
+$ node index.js
+```
+
+<br>
+
+## 4 - The cli requires that you input a path to the records file in csv format, input the included file.csv
+
+```sh
+$ Enter the path to the CSV file please: file.csv
+```
+
+## 5 - The cli will return the required averaged, minimums, and maximums in the console itself
+
+<br>
+
+<img src="./outputDataObject%20preview.jpeg">
+
+<br>
--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// output
-// 1. Total average BTC/USD value for entire period (December 2015 to February 2016)  
-// 2. Average BTC/USD value for each month (December, January, February)  
-// 3. Maximum BTC/USD value for each month (December, January, February)  
-// 4. Minimum BTC/USD value for each month (December, January, February)
-
 const fs = require('fs')
 const readline = require('readline').createInterface({
     input: process.stdin,
@@ -13,29 +7,77 @@ const readline = require('readline').createInterface({
 //when done reading prompt exit program 
 readline.on('close', () => process.exit(1))
 
-
 const prompt = (query) => new Promise((resolve) => readline.question(query, resolve));
 
 // IIFE pattern
 (async () => {
     try {
-        let totalAverage, averagesPerMonth, maximumPerMonth, minimumPerMonth
-        const pathToFile = await prompt('Enter the path to the CSV file please:')
-        let results = []
-        const final = fs.createReadStream(pathToFile)
-            .on('error', (error) => {
-                console.log(error)
-            })
-            // .pipe(csv())
-            .on('data', (chunk) => results.push(Buffer.from(chunk)))
-            .on('end', () => resolve(Buffer.concat(results).toString('utf8')));
+        let dataOutputObject = {
+            totalAverage: 0,
+            totalNumberOfAllRecords: 0,
+            totalSumOfAllRecords: 0,
+            months: {}
 
-        const result = await streamToString(stream)
-        console.log(final)
+        }
+
+        // prompt user for file path
+        const pathToFile = await prompt('Enter the path to the CSV file please:')
+        // create file read stream
+        const stream = fs.createReadStream(pathToFile)
+        const data = await streamToString(stream)
+        // split data into records aka rows
+        const splitRows = data.split(/\r?\n/)
+
+        // process each record, get the final needed values in one pass
+        splitRows.forEach((row, index) => {
+            // exclude the first row, titles
+            if (index === 0) return
+
+            const pair = row.split(",")
+            const month = pair[0]?.split('/')[0]
+            if (month) {
+                // initialize month if not in the object already
+                if (!dataOutputObject.months[month]) {
+                    dataOutputObject.months[month] = {
+                        recordsNumberForThisMonth: 0,
+                        recordsSumForThisMonth: 0,
+                        maxValueOfThisMonth: Number.MIN_VALUE,
+                        minValueOfThisMonth: Number.MAX_VALUE,
+                        averageOfThisMonth: 0
+                    }
+                }
+
+                // account for the records in the total
+                dataOutputObject.totalNumberOfAllRecords++
+                dataOutputObject.totalSumOfAllRecords += Number(pair[1])
+
+                // account for the records in the corresponding month
+                dataOutputObject.months[month].recordsNumberForThisMonth++
+                dataOutputObject.months[month].recordsSumForThisMonth += Number(pair[1])
+                { (dataOutputObject.months[month].maxValueOfThisMonth < pair[1]) && (dataOutputObject.months[month].maxValueOfThisMonth = pair[1]) }
+                { (dataOutputObject.months[month].minValueOfThisMonth > pair[1]) && (dataOutputObject.months[month].minValueOfThisMonth = pair[1]) }
+
+            }
+        })
+        // calculate averages
+        dataOutputObject.totalAverage = dataOutputObject.totalSumOfAllRecords / dataOutputObject.totalNumberOfAllRecords
+        Object.keys(dataOutputObject.months).forEach((month) => {
+            dataOutputObject.months[month].averageOfThisMonth = dataOutputObject.months[month].recordsSumForThisMonth / dataOutputObject.months[month].recordsNumberForThisMonth
+        })
+        console.log("\r\n\r\n")
+        console.log("Here are your final results:\r\n\r\n", dataOutputObject)
         readline.close()
     } catch (e) {
         console.error("unable to prompt", e)
     }
 })()
 
-
+// takes a ReadStream and returns data as string
+function streamToString(stream) {
+    const chunks = [];
+    return new Promise((resolve, reject) => {
+        stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
+        stream.on('error', (err) => reject(err));
+        stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
+    })
+}
